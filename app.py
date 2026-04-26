e6e8423349b605759e5f3c845f6377c3648bfd79
faithwalkergaming <faithwalkergaming@gmail.com>
2026-04-26 01:11:45 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+API_URL = "https://api.gametools.network/bf6/servers/?name=faithwalker&limit=50"
+
+@app.route("/")
+def total_players():
+    try:
+        r = requests.get(API_URL, timeout=10)
+        data = r.json()
+
+        servers = data.get("servers", [])
+
+        total = sum(
+            int(s.get("playerAmount", 0))
+            for s in servers
+            if int(s.get("playerAmount", 0)) > 0
+        )
+
+        return jsonify({"count": total})
+
+    except:
+        return jsonify({"count": 0})
